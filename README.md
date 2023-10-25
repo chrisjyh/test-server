d14ed0c1947753d5bbcf9a40443fec18e0bc5195
jooeunho <pureredgrace@gmail.com>
2023-10-25 17:33:22 +0900

    todo connect db

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # test-server
 여러가지 기능 테스트 용 서버
+
+
+### 실행
+--reload : 코드 변경후 서버 재시작
+
+
+``` 
+uvicorn main:app --reload
+```
+
+### api 문서 보기
+ 대화형 : http://127.0.0.1:8000/docs <br>
+ 대안형 : http://127.0.0.1:8000/redoc
+
+### mysql 도커 연결
+-p: 포트 맵핑
+-p 3306:3306 : 호스트의 3306 와 컨테이너 안의 3306번 포트 연결
+-e : 환경변수 지정
+-d : detach 옵션 (백그라운드에서 동작하게 하는 옵션)
+-v : 볼륨옵션 (컨테이너를 종료하게되면 데이터 모두 삭제됨) 
+    아래의 코드는 로컬 데이터베이스(todos)라는 곳에 저장
+
+- docker ps : 이미지 확인
+- dokcer logs todos: todos라는 컨테이너의 로그 확인
+- docker volume ls: 도커 이미지의 볼륨
+- docker exec -it todos bash: todos의 배쉬 사용
+```
+# 로컬에 3306포트를 사용하고 있어 3307포트로 연결
+docker run -p 3307:3306 -e MYSQL_ROOT_PASSWORD=1234 -e MYSQL_DATABASE=todos -d -v todos:/db --name todos mysql:8.0
+
+docker ps
+
+docker logs todos
+
+docker volume ls
+
+docker exec -it todos bash
+```
--- src/main.py
+++ src/main.py
@@ -1,36 +1,29 @@
-from fastapi import FastAPI, Body, HTTPException
+from typing import List
+
+from fastapi import FastAPI, Body, HTTPException, Depends
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from database.connection import get_db
+from database.orm import ToDo
+from database.repository import get_todos
 
 app =FastAPI()
-
+todo_data = {}
 @app.get("/")
 def health_check_handler():
     return {"ping": "pong"}
 
-todo_data ={
-    1: {
-        "id": 1,
-        "contents": "가나다라",
-        "is_done": False,
-    },
-    2: {
-        "id": 2,
-        "contents": "뮻ㅊㅊㅊ",
-        "is_done": False,
-    },
-    3: {
-        "id": 3,
-        "contents": "ㅁㅁㅁㅁㅁㅁㅁ",
-        "is_done": True,
-    },
-}
-
 @app.get("/todos")
-def get_todos_handler(order: str = None):
-    ret = list(todo_data.values())
+def get_todos_handler(
+    order: str = None,
+    session: Session = Depends(get_db),
+):
+    todos: List[ToDo] = get_todos(session= session)
+
     if order == "DESC":
-        return ret[::-1]
-    return ret
+        return todos[::-1]
+    return todos
 
 @app.get("/todos/{todo_id}", status_code=200)
 def get_todos_handler(todo_id: int):
